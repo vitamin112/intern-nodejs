0ee0a894b500b3824fde67b60f15b417126af5fd
vitamin112 <xuboom12345@gmail.com>
2023-11-21 23:35:33 +0700

    week-1

--- koaJS/src/app.js
+++ koaJS/src/app.js
@@ -1,29 +1,36 @@
 import Koa from "koa";
 import { koaBody } from "koa-body";
 import render from "koa-ejs";
-import path from "path";
-import { URL } from "url";
+import Router from "koa-router";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { getAll } from "./database/productRepository.js";
 import { routes } from "./routes/routes.js";
 
+const router = new Router();
 const app = new Koa();
-const __dirname = new URL(".", import.meta.url).pathname;
+const PORT = 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 render(app, {
-  root: path.join(__dirname, "view"),
-  layout: "template",
+  root: `${__dirname}/view`,
+  layout: false,
   viewExt: "html",
   cache: false,
-  debug: true,
 });
 
-app.use(async function (ctx) {
-  await ctx.render("product", {});
+router.get("/", async (ctx) => {
+  const products = await getAll();
+  await ctx.render("product", { products });
 });
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.use(koaBody());
 app.use(routes.routes());
 app.use(routes.allowedMethods());
-const PORT = 5000;
 
 app.listen(PORT);
 console.log("app listening on port " + PORT);
