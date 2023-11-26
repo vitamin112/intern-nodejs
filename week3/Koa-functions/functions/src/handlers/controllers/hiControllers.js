1d3ecb1478c3989ebc7d8f89667fadd61cebc97b
vitamin112 <xuboom12345@gmail.com>
2023-11-26 22:30:03 +0700

    -

--- week3/Koa-functions/functions/src/handlers/controllers/hiControllers.js
+++
@@ -1,9 +0,0 @@
-const sayHi = async (ctx) => {
-  return (ctx.body = {
-    Message: "Hi",
-  });
-};
-
-module.exports = {
-  sayHi,
-};
--- week3/Koa-functions/functions/src/index.js
+++
@@ -1,4 +0,0 @@
-const functions = require("firebase-functions");
-const apiHandler = require("./handlers/api");
-
-exports.helloWorld = functions.https.onRequest(apiHandler.callback());
